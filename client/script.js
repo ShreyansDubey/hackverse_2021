8c0da1a8ab1ef967408de18305b4092006517350
blurrrb <17aakashsingh1999@gmail.com>
2021-02-27 21:15:57 +0530

    client code

--- client/script.js
+++ client/script.js
@@ -5,17 +5,13 @@ class CoordMapType {
         this.minZoom = 10;
     }
     getTile(coord, zoom, ownerDocument) {
-
-        console.log(coord, zoom)
-        const div = ownerDocument.createElement("div");
-        div.innerHTML = String(coord);
-        div.style.width = this.tileSize.width + "px";
-        div.style.height = this.tileSize.height + "px";
-        div.style.fontSize = "10";
-        div.style.borderStyle = "solid";
-        div.style.borderWidth = "1px";
-        div.style.borderColor = "#AAAAAA";
-        return div;
+        const img = ownerDocument.createElement('img');
+        img.src = `http://localhost:5000/tile?x=${coord.x}&y=${coord.y}&zoom=${zoom}`
+        img.alt = `${coord.x} ${coord.y} ${zoom}`;
+        img.style.width = this.tileSize.width + "px";
+        img.style.height = this.tileSize.height + "px";
+        img.style.opacity = "40%";
+        return img;
     }
     releaseTile(tile) {}
 }
@@ -24,8 +20,8 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: {
-            lat: 41.85,
-            lng: -87.65
+            lat: 12.9716,
+            lng: 77.5946
         },
     });
     
